7a78ef75f84c5c8399e9478adb1aa7e6e5148367
anumfatima362 <116074863+anumfatima362@users.noreply.github.com>
2023-04-26 18:46:09 +0500

    Add files via upload

---
+++ text_description_constants.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+const String kMinarDesc =
+    '''Minar e pakistan is a historical monument in lahore. The tower was built 1960 and 1968 on the site where the All-india Muslim league 
+Passed the resolution on 23 March 1940 . It was held at Minto Park  now renamed as
+iqbal park The nation celebrates this momentous occasion as Pakistan Day and  
+the government of pakistan announces it as a public holiday every year. 
+The history of Minar e pakistan holds great importance. ''';
+
+const String kBadshahiDesc =
+    '''The Badshahi Mosque was built in 1671-1673 by the Mughal Emperior Aurangzab. Badshahi Mosque 
+Shows the  best combination of architecture and art work as well as reflects the skills and abilities of the past people. It holds the title of 2nd biggest mosque of Pakistan. Badshahi Mosque is capable more than 5000 Namazi at a time. There are one main and two joining domes of the mosque. The beautiful domes are the proud of Badshahi Mosque. 
+''';
+
+const String kSheeshMahalDesc =
+    '''Sheesh mahal and also called as the mirror palace is one of the refined magnificent piece of architecture built with beautiful precious stones and glass, coated with beautiful handmade paintings. Sheesh mahal is the most popular and beautiful part of Amer Fort. The sheesh Mahal is the finest of several white marble pavillions erected by shah jahan in 1631-2.''';
+
+const kDehliGateDesc =
+    '''Delhi Gate is one of six remaining historic gates of the Walled City of Lahore, Pakistan. Delhi Gate and the adjacent Shahi Hammam were restored in 2015 by the Aga Khan Cultural Service Pakistan. The gate itself was also destroyed by the British, but was recontructed in the 19th centuary under the british raj. ''';
+
+const kMazarEQuaidDesc =
+    '''The final resting place of Quaid-e-Azam Muhammad Ali Jinnah is situated in the middle of the city of Karachi. Its humongous and well lit presence is one of the attractive tourist spots in Sindh for everyone. Always crowded with people, Mazar e Quaid is the perfect spot to observe the different kinds of Pakistanis. ''';
+
+const kMoenJoDaroDesc =
+    '''Something that all of us have read about in our Pakistan Studies curriculum very thoroughly, but know very little about. 
+
+Located in Harappa, Mohenjo daro literally translates to “the mound of dead” . It is one of the most intriguing mysteries and also morbidly beautiful. Being there makes you feel like you’ve been transported. The intricate civilization and its structures and organization of the irrigation of sewage system is considered ahead of their times. Even the cultural advancements that have been studied, have seem to be more advanced than any other place at that time! 
+
+The structure is now threatened by erosion due to improper preservation. ''';
+
+const kCliftonDesc =
+    '''Clifton Beach, at the northwest tip of South Africa’s Cape Peninsula, is a trendy seaside retreat for international hipsters and jet-setters. The entire Clifton beach is vast and clean, with white sand and a large number of tanned bodies reflected in the brilliant African sunshine. ''';
+
+const kChaukandiDesc =
+    ''' Situated east of Karachi near the National Highway, the Chaukhandi Tombs are basically an Islamic cemetery full of beautifully carved stonework. The eerie feeling around the graveyard surrounded with beautiful stones is a unique combination and perfect representation of Sindh culture.''';
+const kKaghanValleyDesc =
+    '''The kaghan valley is an alpine valley located in the Mansehra District  of khyber Pakhtunkhawa ,pakistan. The valley covers a distance 155 kilometers across northen pakistan 
+
+Rising from its lowest elevation of 650 m to its highest point at the Babusar pass around 4,1770m. ''';
+
+const kKhyberPassDesc =
+    '''  The Khyber Pass is a mountain pass in the Khyber Pakhtunkhwa province of Pakistan, on the border with Afghanistan . It connects the town of Landi Kotal to the Valley of Peshawar at Jamrud by traversing part of the Spin Ghar mountains.  The name Khyber is also applied to the range through which the pass runs—a series of arid broken hills that form the last spurs of the Spīn Ghar (Pashto: “White Mountains”) Range.  ''';
+
+const kKalashValleyDesc =
+    '''The Kalash Valley or Wadi e Kalash is located at the altitude of 1650m above sea level in the District Chitral of KPK in Pakistan. It comprises of spectacular small valleys viz. Birir, Bumborate and Rumboor. These valleys reside in the middle of the mighty Hindu Kush Mountains among the sweetly scented Oak trees and splendid florae. ''';
+
+const kMukehspuriDesc =
+    '''Mushkpuri, is a 2,800-metre-high mountain in the Nathia Gali Hills, in the Abbottabad District of the Khyber Pakhtunkhwa province in northern Pakistan. It is 90 kilometres north of Islamabad, just above Dunga Gali in the Nathia Gali area of Ayubia National Park. It is the second highest peak in the Galyat Region after Miranjani which is located at 2,992 metres. Much of it the mountain is covered with Western Himalayan subalpine conifer forests.  ''';
+
+const kQuaidEAzamResDesc =
+    ''' A city in the Ziarat district of Baluchistan, Pakistan, Ziarat is a very famous tourist destination.  Ziarat is at a distance of 130 kilometers from the Quetta. Quaid-e-Azam Residency is also situated in Ziarat where the great leader Quaid-e-Azam spent his last days before the departure from this world.   
+
+Quaid-e-Azam Residency is renowned as Ziarat Residency. The residency is a very famous part of the city. Ziarat is the most beautiful district of Pakistan. Thousands of people from all around come here for tourism and picnic. Jinnah Residency situated in Ziarat attracts many tourists to Ziarat. People from all around the world come here to witness the memories of Quaid-e-Azam. ''';
+
+const kPrincessOfHopeDesc =
+    '''The Princess of Hope is a natural rock formation in Pakistan of the type known as a hoodoo or "fairy chimney" and which could fancifully be construed as resembling a crowned and skirted female figure looking toward the horizon .Princess of Hope is a rock formation in southern part of Baluchistan, Pakistan. Heavy rainfall in the region and winds blowing from the ocean have shaped the mountainous rock into beautiful, natural sculptures. One of these figures looks like a lady standing tall, looking over the horizons. This figure is called Princess of Hope, depicting a princess standing tall, looking for something, probably hope as they say.''';
+
+const kPirGhaibDesc =
+    ''' Pir Ghaib Waterfall Balochistan are beautiful and majestic waterfalls in the Bolan Valley, 70 kilometers (43 mi) from Quetta in Balochistan, Pakistan. Here, the waterfall cascades down the rocky mountainside and weaves its way through many streams and lakes among shady palm trees. pir ghaib waterfall balochistan . ''';
+
+const kMaulaChotokDesc =
+    '''A beautiful village called Moola is located more than 80 km from Khuzdar. It is situated in Tehsil Khuzdar. As the gateway between central and west Baluchistan, ssssss The shortest distance between central Balochistan and Sindh is avoided by using this route (Moola Pass). Ancient ruins from Thorekhear, Hatachi, Hayrav, Kial Beig, and Pasta Khan suggest that Moola Chotok belongs to this civilization of 2,000 years ago. There is a river named Moola Chotok in Jalawan that originates in Dist. Khuzdar. The river also has fish in it, so fishing is a favorite hobby of people in this region.  ''';
+
+const kBaltitFortDesc =
+    '''Baltit Fort is a prominent historical site in Pakistan and this place offers a panoramic view of Hunza Valley and the highest mountain peaks in Pakistan.  
+
+Formerly known as Kanjut, Hunza was an independent principality ruled by a hereditary monarch named Mir Tham in the local Burushaski language. In the 19th century, Baltit Fort was part of the Great Game.    Baltit Fort was the capital of Hunza State, and the old settlement is still intact to this day.  ''';
+
+const kKharphochoFortDesc =
+    '''  Skardu Fort or Karpocho Fort is a fort in Skardu city in Gilgit-Baltistan of the disputed Kashmir region that dates from the 16th century CE. An ancient mosque is also present inside the fort. The fort and its mosque are located on the eastern face of the Khardong hill 40 ft above Skardu city.  
+
+This is the main center of attraction and landmark of the city. This is a wonderful visiting point in Skardu where you can enjoy with your friends and family. This place is very old and famous.  ''';
+
+const kRakaposhiPointDesc =
+    '''A tourist spot in the town of Gilgit (located in the bagrote valley) called "Zero Point of Rakaposhi" is the closest view point of the mountain. Rakaposhi is the only mountain in the world which rises straight from beautifully cultivated fields to the height of 25,550 feet. ''';
+
+const kKhunjerabPassDesc =
+    '''Khunjerab Pass  is a 4,693-meter-high (15,397 ft) mountain pass in the Karakoram Mountains, in a strategic position on the northern border of Pakistan (Gilgit–Baltistan's Hunza and Nagar Districts) and on the southwest border of China (Xinjiang). Mutsjliga Pass is a 5,314-meter-high (17,434 ft) mountain pass at 36.97374°N 75.2973°E near Khunjerab Pass.''';
+
+const kFaisalMosqueDesc =
+    '''Shah Faisal Masjid is located in Islamabad, the Capital of Pakistan. It was named after the King of Saudi Arabia. The King also pay for the construction of the Shah Faisal Mosque. It is one of the largest, unique and faisal mosque history in South Asia. Over 100,000 worshipers can offer prayers in its main hall and courtyard. Shah Faisal Masjid is famous for its architecture, which is a departure from the traditional vault. The building of Masjid like a tent, situated between four minarets.   ''';
+
+const kDamanEKohDesc =
+    '''Daman-e-koh  is a viewing point and hill top garden north of Islamabad and located in the middle of the Margalla Hills. Its name is a conjunction of two Persian words, which together means foot hills. It is about 2400 ft from sea level and almost 500 ft from the city of Islamabad. It is a popular destination for the residents as well as the visitors to the capital. Daman-e-Koh is a midpoint on the road to Pir Sohawa which is located at the top of Margalla Hills at an elevation of about 3600 ft. The popularity of the site regularly leads to traffic jams due to the absence of public transport options on the narrow, winding road. There is a plan to construct a chairlift from Daman-e-Koh to Pir Sohawa  ''';
+
+const kMargallaHillsDesc =
+    ''' The Margalla Hills — also called the Margalla Mountain Range, is a hill range part of the lesser Himalayas located north of Islamabad, Pakistan. Margalla Range has an area of 12,605 hectares. The hills are a part of Murree hills. It is a range with many valleys as well as high mountains.The hill range nestles between an elevation of 685 meters at the western end and 1,604 meters on its east with average height of 1000 meters. Its highest peak is Tilla Charouni. The range gets snowfall in winters.  ''';
+
+const kPakMonumentDesc =
+    '''The Pakistan Monument is a national monument and heritage museum located on the western Shakarparian Hills in Islamabad, Pakistan. The monument was constructed to symbolize the unity of the Pakistani people. The monument was completed in 2006 under the supervision of Engineer Syed Mahmud Khalid. The inauguration ceremony was held on 23rd March 2007. It covers 2.8 hectares of land and holds the first position on the list of national monuments of Pakistan.   ''';
